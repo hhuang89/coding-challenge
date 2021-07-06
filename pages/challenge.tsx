1f6e0a69fd6cc89bb687dcabca33b956a2542f26
hhuang89 <hhua502@aucklanduni.ac.nz>
2021-07-07 00:21:54 +1200

    all calculations done, TODO:formatting

--- pages/challenge.tsx
+++ pages/challenge.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { getData } from "../lib/services/api-services";
 import { IResponse, Datum } from "../lib/model";
-import { REVENUE, EXPENSE, SALES, DEBIT } from "../lib/model";
+import * as constants from "../lib/model";
 
 export default function codeChallenge() {
   const [data, setData] = useState<Datum[]>();
@@ -13,12 +13,13 @@ export default function codeChallenge() {
   }, []);
 
   const revenue = data
-    ?.filter((item) => item.account_category === REVENUE)
+    ?.filter((item) => item.account_category === constants.REVENUE)
     .reduce((acc, cur) => {
       return acc + cur.total_value;
     }, 0);
+
   const expense = data
-    ?.filter((item) => item.account_category === EXPENSE)
+    ?.filter((item) => item.account_category === constants.EXPENSE)
     .reduce((acc, cur) => {
       return acc + cur.total_value;
     }, 0);
@@ -26,24 +27,81 @@ export default function codeChallenge() {
   const calculateGPM = () => {
     const sum = data
       ?.filter(
-        (item) => item.account_type === SALES && item.value_type === DEBIT
+        (item) =>
+          item.account_type === constants.SALES &&
+          item.value_type === constants.DEBIT
       )
       .reduce((acc, cur) => {
         return acc + cur.total_value;
       }, 0);
+      
 
     if (typeof revenue === "number" && typeof sum === "number") {
-      return sum / revenue;
+      return 100 * sum / revenue;
+    } else {
+      return -1;
     }
   };
-
   const GPM = calculateGPM();
+
+  //NPM = (revenue - expense) / revenue
+  const calculateNPM = () => {
+    if (typeof revenue === "number" && typeof expense === "number") {
+      return 100 * (revenue - expense) / revenue;
+    } else {
+      return -1;
+    }
+  };
+  const NPM = calculateNPM();
+
+  //WCR = assets / liabilities
+  const assets = data
+    ?.filter(
+      (item) =>
+        item.account_category === constants.ASSETS &&
+        (item.value_type === constants.DEBIT || constants.CREDIT) &&
+        (item.account_type === constants.CURRENT ||
+          constants.BANK ||
+          constants.RECEIVABLE)
+    )
+    .reduce((acc, cur) => {
+      if (cur.value_type === constants.CREDIT) {
+        return acc - cur.total_value;
+      } else if (cur.value_type === constants.DEBIT) {
+        return acc + cur.total_value;
+      }
+      return 0;
+    }, 0);
+
+  const liabilities = data?.filter(
+    (item) =>
+      item.account_category === constants.LIABILITY &&
+      (item.value_type === constants.DEBIT || constants.CREDIT) && 
+      (item.account_type === constants.CURRENT || constants.PAYABLE)
+  )
+  .reduce((acc, cur) => {
+    if (cur.value_type === constants.CREDIT) {
+      return acc + cur.total_value;
+    } else if (cur.value_type === constants.DEBIT) {
+      return acc - cur.total_value;
+    }
+    return 0;
+  }, 0);
+
+  const calculateWCR = () => {
+    if (typeof assets === "number" && typeof liabilities === "number") {
+      return (assets / liabilities) * 100;
+    }
+  }
+  const WCR = calculateWCR();
 
   return (
     <>
       <div>{revenue}</div>
       <div>{expense}</div>
       <div>{GPM}</div>
+      <div>{NPM}</div>
+      <div>{WCR}</div>
     </>
   );
 }
