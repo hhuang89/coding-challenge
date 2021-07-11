1ad3afc17c7b17531bf7fdea4d86be0c6e5af1c4
hhuang89 <hhua502@aucklanduni.ac.nz>
2021-07-11 22:33:21 +1200

    delete redundant code

--- util/services/api-services.tsx
+++ util/services/api-services.tsx
@@ -5,15 +5,3 @@ import {IResponse} from "../../lib/model/response";
 export function getData(): Promise<IResponse> {
     return axios.get('/data.json').then(res => res.data)
 }
-
-export function testCase1(): Promise<IResponse> {
-    return axios.get('/testcase1.json').then(res => res.data)
-}
-
-export function testCase2(): Promise<IResponse> {
-    return axios.get('/testcase2.json').then(res => res.data)
-}
-
-export function testCase3(): Promise<IResponse> {
-    return axios.get('/testcase3.json').then(res => res.data)
-}
